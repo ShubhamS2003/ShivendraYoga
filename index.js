03358eab1488336703de6585ec57ce19c3b89a48
Kuntash <kuntashtashi11@gmail.com>
2021-11-26 02:29:58 +0530

    Removed auto sliding, added some color scheme to match fireship.io

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const carouselImages = document.querySelectorAll('.slide__image');
 //Length of the carouselImages will be 2 more
 
 let counter = 1;
-const size = carouselImages[0].clientWidth;
+const size = 100;
 
-slideContainer.style.transform = `translateX(${-size * counter}px)`;
+slideContainer.style.transform = `translateX(${-size * counter}%)`;
 
 const slideLeft = () => {
     if(counter >= carouselImages.length - 1) return;
     slideContainer.style.transition = `transform 0.8s ease-in-out`;
     counter++;
-    slideContainer.style.transform = `translateX(${-size * counter}px)`; 
+    slideContainer.style.transform = `translateX(${-size * counter}%)`; 
 }
 
 const slideRight = () => {
@@ -22,7 +22,7 @@ const slideRight = () => {
     slideContainer.style.transition = `transform 0.8s ease-in-out`;
     counter--;
     console.log(counter);
-    slideContainer.style.transform = `translateX(${-size * counter}px)`;
+    slideContainer.style.transform = `translateX(${-size * counter}%)`;
 }
 nextBtn.addEventListener('click', slideLeft);
 prevBtn.addEventListener('click', slideRight);
@@ -32,15 +32,15 @@ slideContainer.addEventListener('transitionend', () => {
     {
         slideContainer.style.transition = `none`;
         counter = carouselImages.length - 2;
-        slideContainer.style.transform = `translateX(${-size * counter}px)`;
+        slideContainer.style.transform = `translateX(${-size * counter}%)`;
     }
     if (counter == carouselImages.length - 1)
     {
         slideContainer.style.transition = `none`;
         console.log(`none`);
         counter = 1;
-        slideContainer.style.transform = `translateX(${-size * counter}px)`;
+        slideContainer.style.transform = `translateX(${-size * counter}%)`;
     }
 })
 // Timer function
-setInterval(slideLeft, 2000);
+// setInterval(slideLeft, 2000);
