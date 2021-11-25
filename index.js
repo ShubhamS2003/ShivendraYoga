c5baa7b7b473d44c279ea896a984f457170f187a
Kuntash <kuntashtashi11@gmail.com>
2021-11-26 01:33:02 +0530

    Slider working, with automatic sliding after 2sec

--- index.js
+++ index.js
@@ -1,20 +1,46 @@
-const imgArray = ['slide__image1', 'slide__image2', 'slide__image3'];
-const dir = `Assets/Images/`;
 const prevBtn = document.querySelector('.btn__prev');
 const nextBtn = document.querySelector('.btn__next');
-console.log(prevBtn);
-let currentSlide = 0;
-let nextSlide = currentSlide + 1;
-const totalSlide = 3;
-prevBtn.addEventListener('click', function()
-{
-    console.log(currentSlide, nextSlide);
 
-    const currentImg = document.querySelector(`.${imgArray[currentSlide]}`);
-    const nextImg = document.querySelector(`.${imgArray[nextSlide]}`);
-    currentImg.style.transform = `translateX(-100%)`;
-    nextImg.style.transform = `translateX(-100%)`;
-    currentSlide = (currentSlide + 1) % 3;
-    nextSlide = (currentSlide + 1) % 3;
-    
-});
+const slideContainer = document.querySelector('.slide__container');
+const carouselImages = document.querySelectorAll('.slide__image');
+//Length of the carouselImages will be 2 more
+
+let counter = 1;
+const size = carouselImages[0].clientWidth;
+
+slideContainer.style.transform = `translateX(${-size * counter}px)`;
+
+const slideLeft = () => {
+    if(counter >= carouselImages.length - 1) return;
+    slideContainer.style.transition = `transform 0.8s ease-in-out`;
+    counter++;
+    slideContainer.style.transform = `translateX(${-size * counter}px)`; 
+}
+
+const slideRight = () => {
+    if(counter <= 0) return;
+    slideContainer.style.transition = `transform 0.8s ease-in-out`;
+    counter--;
+    console.log(counter);
+    slideContainer.style.transform = `translateX(${-size * counter}px)`;
+}
+nextBtn.addEventListener('click', slideLeft);
+prevBtn.addEventListener('click', slideRight);
+
+slideContainer.addEventListener('transitionend', () => {
+    if (counter == 0)
+    {
+        slideContainer.style.transition = `none`;
+        counter = carouselImages.length - 2;
+        slideContainer.style.transform = `translateX(${-size * counter}px)`;
+    }
+    if (counter == carouselImages.length - 1)
+    {
+        slideContainer.style.transition = `none`;
+        console.log(`none`);
+        counter = 1;
+        slideContainer.style.transform = `translateX(${-size * counter}px)`;
+    }
+})
+// Timer function
+setInterval(slideLeft, 2000);
